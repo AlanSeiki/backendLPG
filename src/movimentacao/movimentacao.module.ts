a8ef032f6515c9781630bcee83e3e77bee6851c8
Alan Seiki Arima <147749349+AlanSeiki@users.noreply.github.com>
2024-05-25 17:30:48 -0300

    ajuste seed e conectando com app

--- src/movimentacao/movimentacao.module.ts
+++ src/movimentacao/movimentacao.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MovimentacaoService } from './movimentacao.service';
 import { MovimentacaoController } from './movimentacao.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movimentacao } from './entities/movimentacao.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Movimentacao])],
   controllers: [MovimentacaoController],
   providers: [MovimentacaoService],
+  exports:[MovimentacaoService]
 })
 export class MovimentacaoModule {}
--- src/movimentacao/movimentacao.service.ts
+++ src/movimentacao/movimentacao.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMovimentacaoDto } from './dto/create-movimentacao.dto';
 import { UpdateMovimentacaoDto } from './dto/update-movimentacao.dto';
+import { Movimentacao } from './entities/movimentacao.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class MovimentacaoService {
+  constructor(
+    @InjectRepository(Movimentacao)
+    private movimentacaoRepository: Repository<Movimentacao>
+  ){
+
+  }
   create(createMovimentacaoDto: CreateMovimentacaoDto) {
     return 'This action adds a new movimentacao';
   }
 
   findAll() {
-    return `This action returns all movimentacao`;
+    return this.movimentacaoRepository.find({order:{'data':'ASC'}});
   }
 
   findOne(id: number) {
